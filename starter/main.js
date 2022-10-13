7a53dffd05cabdb0bbfe4c27cb9cac67d34d6c10
Agboola2019 <amorioloye@gmail.com>
2022-10-13 05:54:21 -0700

    completed mobile version

---
+++ starter/main.js
@@ -0,0 +1,26 @@
+
+
+
+// ES5
+
+// console. log("send me your account number")
+// alert( writeup)
+// var writeup = " i am hungry"
+
+// function dosomething() {
+//               alert( writeup)
+// }
+
+// console.log(document)
+// console.log(hamburgerdiv)
+
+
+
+
+var hamburgerdiv = document.querySelector(".hamburger")
+
+
+
+function dosomething () {
+hamburgerdiv.classList.toggle ("showburger")
+}

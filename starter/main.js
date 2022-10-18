3ccf21fbcba966b0d8a1deb9c1052cf5c71eb36e
Agboola2019 <amorioloye@gmail.com>
2022-10-18 01:54:34 -0700

    Completed header for large screen

--- starter/main.js
+++ starter/main.js
@@ -16,11 +16,18 @@
 
 
 
-
 var hamburgerdiv = document.querySelector(".hamburger")
+var mobileLinks = document.querySelector(".mobile-links-holder ")
+var mybackdrop = document.querySelector(".backdrop ")
 
-
-
-function dosomething () {
-hamburgerdiv.classList.toggle ("showburger")
+function dosomething() {
+              hamburgerdiv.classList.toggle("showburger")
+              mobileLinks.classList.toggle("show-mobile-links-holder")
+              mybackdrop.classList.toggle("show-backdrop")
 }
+
+
+
+
+
+
